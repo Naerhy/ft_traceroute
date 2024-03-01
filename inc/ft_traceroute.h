6f17e61d88490cfaa7f71a9f2241078425764003
Naerhy <naerhy@protonmail.com>
2024-03-01 11:01:21 +0100

    Parse host arguments

--- inc/ft_traceroute.h
+++ inc/ft_traceroute.h
@@ -1,4 +1,15 @@
 #ifndef FT_TRACEROUTE_H
 #define FT_TRACEROUTE_H
 
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+typedef struct Traceroute
+{
+	char* host;
+	int sockfd;
+} Traceroute;
+
 #endif
--- src/ft_traceroute.c
+++ src/ft_traceroute.c
@@ -1,6 +1,22 @@
 #include "ft_traceroute.h"
 
-int main(void)
+static int exit_traceroute(char const* errstr)
 {
+	printf("ft_ping: %s\n", errstr);
+	return errstr ? 1 : 0;
+}
+
+static void parse_args(int argc, char** argv, Traceroute* traceroute)
+{
+	traceroute->host = *(argv + argc - 1);
+}
+
+int main(int argc, char** argv)
+{
+	Traceroute traceroute;
+
+	if (argc < 2)
+		return exit_traceroute("missing host operand");
+	parse_args(argc, argv, &traceroute);
 	return 0;
 }
